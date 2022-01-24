b13bd909b8fe845cbeb506991fd4b060a9c3d35b
Zackary Santana <ZackZackYack@gmail.com>
2022-01-24 11:49:51 -0500

    Fixed formatting and added logic and separation of methods. As well, changed document ID (didn't work for windows)

--- injection.js
+++ injection.js
@@ -1,34 +1,53 @@
 const githubContent = `
 
-<section id="ember311" class="artdeco-card ember-view break-words pb3
-      
-mt4"><!---->
+<section id="ember311" class="artdeco-card ember-view break-words pb3 mt4">
+    <div class="pvs-header__container">
+      <div class="pvs-header__top-container--no-stack">
+        <div class="pvs-header__left-container--stack">
+          <div class="pvs-header__title-container">
+            <h2 class="pv-profile-section__card-heading pvs-header__title">
+              <!---->
+              <span aria-hidden="true">GitHub</span>
+              <!---->
+            </h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>`;
 
-<div id="experience" class="pv-profile-card-anchor"></div>
-      <!---->
-<div class="pvs-header__container">
-<div class="pvs-header__top-container--no-stack">
-<div class="pvs-header__left-container--stack">
-<div class="pvs-header__title-container">
-    <h2 class="pvs-header__title text-heading-large">
-      <span aria-hidden="true"><!---->GitHub<!----></span><span class="visually-hidden"><!---->GitHub<!----></span>
-    </h2>
-<!----><!---->      </div>
+const createGitHubSection = () => {
+  const element = document.createElement('div');
+  element.className = "github-section";
+  element.innerHTML = githubContent;
+  return element;
+}
 
-<!---->    </div>
+const findExperienceSection = () => {
+    // Zackary's machine (Windows 10) has the element ID as oc-background-section
+    // Replaced the original element ID as experience (possibly what they send for IOS??)
+    const experienceSection = document.getElementById("oc-background-section");
 
-<!---->  </div></div></section>` 
+    if (experienceSection == null) {
+      experienceSection = document.getElementById("experience");
+    }
+    return experienceSection;
+}
 
 const init = () => {
-    const element = document.createElement('div');
-    element.className = "github-section";
-    element.innerHTML = githubContent;
+  // Create GitHub Section
+  const ghSection = createGitHubSection();
+  // Grab experience section
+  const expSection = findExperienceSection();
 
-    const experienceSection = document.getElementById("experience").parentNode
-    const mainContainer = experienceSection.parentNode
-    mainContainer.insertBefore(element, experienceSection)
-    
-    console.log("Hello Hack@Campus!")
+  // Inject in to webpage
+  if (expSection != null) {
+    expSection.parentNode.insertBefore(ghSection, expSection);
+    console.log("Injection Success!");
+  } else {
+    console.log("Injection failed :(")
+  }
 }
 
 window.onload = init;
