733935d4d7272eb14983d61d1b7878b3b53c6248
Zackary Santana <ZackZackYack@gmail.com>
2022-01-24 20:25:04 -0500

    Added some template code for the backend

--- linkedout/index.js
+++ linkedout/index.js
@@ -1,17 +1,22 @@
-var express = require('express');
+var express = require("express");
 var app = express();
+const port = 3000;
 
 // respond with "hello world" when a GET request is made to the homepage
-app.get('/', function (req, res) {
-  res.send('hello world');
-})
+app.get("/", function (req, res) {
+  res.send("hello world");
+});
 
 // GET method route
-app.get('/', function (req, res) {
-    res.send('GET request to the homepage')
-  })
-  
-  // POST method route
-  app.post('/', function (req, res) {
-    res.send('POST request to the homepage')
-  })
+app.get("/", function (req, res) {
+  res.send("GET request to the homepage");
+});
+
+// POST method route
+app.post("/", function (req, res) {
+  res.send("POST request to the homepage");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
