3a64c0d1edc5fbb87daae7f3ba522cf9ba32b5a4
Ivan A. Reyes <42950201+ItsLaro@users.noreply.github.com>
2022-01-25 01:36:15 -0500

    Added preeliminary route to fetch and return pinned repos of a hardcoded user

--- linkedout/index.js
+++ linkedout/index.js
@@ -1,6 +1,9 @@
-var express = require("express");
+import fetch from 'node-fetch';
+import express from 'express';
+
 var app = express();
 const port = 3000;
+const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get("/", function (req, res) {
@@ -10,6 +13,38 @@ app.get("/", function (req, res) {
 // GET method route
 app.get("/", function (req, res) {
   res.send("GET request to the homepage");
+});
+
+// GET method for pinned repos
+app.get("/pinned", function (req, res) {
+
+  const accessToken = GITHUB_TOKEN;
+
+  const query = `
+    query {
+      user(login:"ItsLaro") {
+          pinnedItems(first: 4, types: [REPOSITORY, GIST]) {
+              totalCount
+              edges {
+                  node {
+                      ... on Repository {
+                      name
+                      }
+                  }
+              }
+          }
+      }
+    }`;
+
+  fetch('https://api.github.com/graphql', {
+  method: 'POST',
+  body: JSON.stringify({query}),
+  headers: {
+    'Authorization': `Bearer ${accessToken}`,
+  },
+}).then(res => res.text())
+  .then(body => res.send(body)) 
+  .catch(error => console.error(error));
 });
 
 // POST method route
