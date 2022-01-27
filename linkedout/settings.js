fdcd77ca6fbbf8a2bac3987b032d579e18bfe73b
Alex Lecusay <alex.lecusay@gmail.com>
2022-01-26 23:22:03 -0500

    started settings

--- linkedout/settings.js
+++ linkedout/settings.js
@@ -3,10 +3,21 @@ const router = express.Router();
 
 /* Gets the user settings */
 // To use: /settings/zackary-santana
-router.get("/", function (req, res) {
+router.get("/:linkedin_tag", function (req, res) {
+  var docRef = db.collection("users").doc(req.params.linkedin_tag);
+
+docRef.get().then((doc) => {
+    if (doc.exists) {
+        console.log("Document data:", doc.data());
+    } else {
+        // doc.data() will be undefined in this case
+        console.log("No such document!");
+    }
+}).catch((error) => {
+    console.log("Error getting document:", error);
+});
   // return gh_username from firebase
   // using req.params.linkedin_tag
-  res.send("This is a response");
 });
 
 /* Change user settings */
