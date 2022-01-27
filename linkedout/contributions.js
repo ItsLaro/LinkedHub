c1fbec1686595c59136fac5cbc2f76b5e7a9b634
Zackary Santana <ZackZackYack@gmail.com>
2022-01-26 21:19:30 -0500

    Added database connection and some start to it

--- linkedout/contributions.js
+++ linkedout/contributions.js
@@ -1,9 +1,11 @@
 import express from "express";
+import { putSettings } from "./db.js";
 const router = express.Router();
 
 /* Get contributions */
 router.get("/", function (req, res) {
   res.send("Hey");
+  putSettings("alex-lecusay-0a51a5217", "alexlecusay", []);
 });
 
 export default router;
---
+++ linkedout/db.js
@@ -0,0 +1,25 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, addDoc } from "firebase/firestore";
+initializeApp({
+  apiKey: process.env.API_KEY,
+  authDomain: `${process.env.PROJECT_ID}.firebaseapp.com`,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: `${process.env.PROJECT_ID}.appspot.com`,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+});
+const db = getFirestore();
+
+export async function putSettings(in_tag, gh_username, projects) {
+  try {
+    const docRef = await addDoc(collection(db, "users"), {
+      in_tag,
+      gh_username,
+      projects,
+    });
+    console.log("Document written with ID: ", docRef);
+    return docRef;
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
+}
