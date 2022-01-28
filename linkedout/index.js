572d2c1c9e516e0bf6e745df0ace4a8aff77836b
Alex Lecusay <alex.lecusay@gmail.com>
2022-01-27 22:10:43 -0500

    Added contributions

--- linkedout/index.js
+++ linkedout/index.js
@@ -4,6 +4,7 @@ import cors from "cors";
 
 import settings from "./settings.js";
 import projects from "./projects.js";
+import contributions from "./contributions.js";
 
 const app = express(),
   port = 3000;
@@ -12,6 +13,7 @@ app.use(cors());
 
 app.use("/settings/", settings);
 app.use("/projects/", projects);
+app.use("/contributions/", contributions);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
