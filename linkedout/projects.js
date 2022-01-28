1f62ce52cc9a9807cfcd67bc8cc60e7188ee8426
Zackary Santana <ZackZackYack@gmail.com>
2022-01-27 21:20:28 -0500

    Pushing for Ivan

--- linkedout/projects.js
+++ linkedout/projects.js
@@ -1,8 +1,7 @@
 import fetch from "node-fetch";
 import express from "express";
-import { putDefaultProjects } from "./db.js";
-import { doc, getDoc } from "firebase/firestore";
-import { db } from "./db.js";
+import { getProject, putDefaultProjects, db } from "./db.js";
+import { doc, getDoc, setDoc } from "firebase/firestore";
 const router = express.Router();
 
 /* Gets the projects */
@@ -13,6 +12,31 @@ router.get("/:linkedin_tag", async function (req, res) {
     const data = docSnap.data();
     if (data.hasOwnProperty("projects")) {
       res.json({
+        gh_username: data.gh_username,
+        projects: data.projects,
+      });
+    } else {
+      putDefaultProjects(req.params.linkedin_tag, data.gh_username, res);
+    }
+  }
+});
+
+/* Puts different projects */
+router.post("/:linkedin_tag", async function (req, res) {
+  const docSnap = await getDoc(doc(db, "users", req.params.linkedin_tag));
+
+  if (docSnap.exists()) {
+    const data = docSnap.data(),
+      body = JSON.parse(req.body),
+      replaceIndex = parseInt(body.id),
+      project_name = body.project_name;
+
+    const newProject = await getProject(data.gh_username, project_name);
+    body.projects[replaceIndex] = newProject;
+    setDoc(doc(db, "users", req.params.linkedin_tag), {});
+    if (data.hasOwnProperty("projects")) {
+      res.json({
+        gh_username: data.gh_username,
         projects: data.projects,
       });
     } else {
